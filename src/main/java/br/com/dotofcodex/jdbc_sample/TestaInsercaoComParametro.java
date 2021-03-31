b69ea9bd5efe7f993b62287a9cad88b8f2c64416
Pedro Ferreira <pedroferreiracjr@gmail.com>
2021-03-30 21:51:10 -0300

    implementação de exemplo de remoção de auto-commit automatico da conexão.

--- src/main/java/br/com/dotofcodex/jdbc_sample/TestaInsercaoComParametro.java
+++ src/main/java/br/com/dotofcodex/jdbc_sample/TestaInsercaoComParametro.java
@@ -17,27 +17,37 @@ public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) {
 		logger.info("Loja Virtual - Inserindo produtos na base loja_virtual.");
-		inserirProduto("Mouse", "Mouse Sem Fio");
+		inserirProduto();
 		logger.info("Loja Virtual - Execução concluída.");
 	}
 
-	private static void inserirProduto(String nome, String descricao) {
+	private static void inserirProduto() {
 		try (final Connection conn = ConnectionFactory.getInstance().getConnection()) {
+			conn.setAutoCommit(false);
 			logger.info("Inserindo valores na tabela de produtos.");
 
 			final String sql = "INSERT INTO produto(nome, descricao) VALUES(?, ?);";
 
 			final PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-			stmt.setString(1, nome);
-			stmt.setString(2, descricao);
-
-			final ResultSet result = stmt.getGeneratedKeys();
-			while (result.next()) {
-				Integer id = result.getInt(1);
-				logger.info(String.format("Id gerado para o produto inserido %d.", id));
-			}
+			
+			preencheValores("Smart TV", "42 Polegadas", stmt);
+			preencheValores("SmartPhone", "Samsung", stmt);
 		} catch (SQLException e) {
 			logger.error("Falha na conexão", e);
+		}
+	}
+
+	private static void preencheValores(String nome, String descricao, final PreparedStatement stmt)
+			throws SQLException {
+		logger.info(String.format("Inserindo produto %s...", nome));
+		
+		stmt.setString(1, nome);
+		stmt.setString(2, descricao);
+
+		final ResultSet result = stmt.getGeneratedKeys();
+		while (result.next()) {
+			Integer id = result.getInt(1);
+			logger.info(String.format("Id gerado para o produto inserido %d.", id));
 		}
 	}
 }
